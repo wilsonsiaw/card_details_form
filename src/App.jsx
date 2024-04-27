89f6c3ddb85fbc3098c53b76f2a4a01f8ce83af9
Wilson Siaw <wilsonsiawse@gmail.com>
2024-04-27 20:08:10 +0200

    Put the form in a div

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,42 @@ function App() {
 
   return (
     <div className={styles.container}>
-      <form action="">
-        <label htmlFor="cardholder-name">CARDHOLDER NAME</label>
-        <input 
-          type="text"
-          id='name'
-          placeholder='e.g Jane Appleseed'
-        />
-        <label htmlFor="cardholder-number">CARD NUMBER</label>
-        <input
-          type="number" 
-          id='card-number'
-          placeholder='e.g. 1234 5678 9123 0000'
-        />
-        <div>
-          <label htmlFor="date">EXP. DATE (MM/YY)</label>
+      <div className="form-container">
+        <form action="">
+          <label htmlFor="cardholder-name">CARDHOLDER NAME</label>
+          <input
+            type="text"
+            id='name'
+            placeholder='e.g Jane Appleseed'
+          />
+          <label htmlFor="cardholder-number">CARD NUMBER</label>
           <input
             type="number"
-            id='date'
-            placeholder='MM'
+            id='card-number'
+            placeholder='e.g. 1234 5678 9123 0000'
           />
-          <input
-            type="number"
-            id='date'
-            placeholder='YY'
-          />
-          <label htmlFor="CVC">CVC</label>
-          <input
-            type="number"
-            id='date'
-            placeholder='e.g. 123'
-          />
-        </div>
-        <button>Confirm</button>
-      </form>
+          <div>
+            <label htmlFor="date">EXP. DATE (MM/YY)</label>
+            <input
+              type="number"
+              id='date'
+              placeholder='MM'
+            />
+            <input
+              type="number"
+              id='date'
+              placeholder='YY'
+            />
+            <label htmlFor="CVC">CVC</label>
+            <input
+              type="number"
+              id='date'
+              placeholder='e.g. 123'
+            />
+          </div>
+          <button>Confirm</button>
+        </form>
+      </div>
     </div>
   )
 }
