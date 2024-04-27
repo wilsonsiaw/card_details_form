8b8060b6a4358c3c635bcc8ed8f92c7a8ced3d20
Wilson Siaw <wilsonsiawse@gmail.com>
2024-04-27 21:47:32 +0200

    Change the positioning of the labels in the document

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,36 @@ function App() {
 
   return (
     <div className={styles.container}>
-      <div className="form-container">
+      <div className={styles.formContainer}>
         <form action="">
           <label htmlFor="cardholder-name">CARDHOLDER NAME</label>
           <input
             type="text"
-            id='name'
-            placeholder='e.g Jane Appleseed'
+            id={styles.name}
+            placeholder='e.g. Jane Appleseed'
           />
           <label htmlFor="cardholder-number">CARD NUMBER</label>
           <input
             type="number"
-            id='card-number'
+            id={styles.cardNumber}
             placeholder='e.g. 1234 5678 9123 0000'
           />
-          <div>
-            <label htmlFor="date">EXP. DATE (MM/YY)</label>
+          <label htmlFor="date">EXP. DATE (MM/YY)</label>
+          <label htmlFor="CVC">CVC</label>
+          <div className={styles.expiryInfo}>
             <input
               type="number"
-              id='date'
+              id={styles.dateMonth}
               placeholder='MM'
             />
             <input
               type="number"
-              id='date'
+              id={styles.dateYear}
               placeholder='YY'
             />
-            <label htmlFor="CVC">CVC</label>
             <input
               type="number"
-              id='date'
+              id={styles.cvc}
               placeholder='e.g. 123'
             />
           </div>
