c31c1788d14d720e9324700ccbefb9249bc8e91a
Wilson Siaw <wilsonsiawse@gmail.com>
2024-04-27 19:18:31 +0200

    Add the form element and the inputs

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,42 @@ import './App.css'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <form action="">
+        <label htmlFor="cardholder-name">CARDHOLDER NAME</label>
+        <input 
+          type="text"
+          id='name'
+          placeholder='e.g Jane Appleseed'
+        />
+        <label htmlFor="cardholder-number">CARD NUMBER</label>
+        <input
+          type="number" 
+          id='card-number'
+          placeholder='e.g. 1234 5678 9123 0000'
+        />
+        <div>
+          <label htmlFor="date">EXP. DATE (MM/YY)</label>
+          <input
+            type="number"
+            id='date'
+            placeholder='MM'
+          />
+          <input
+            type="number"
+            id='date'
+            placeholder='YY'
+          />
+          <label htmlFor="CVC">CVC</label>
+          <input
+            type="number"
+            id='date'
+            placeholder='e.g. 123'
+          />
+        </div>
+        <button>Confirm</button>
+      </form>
+    </div>
   )
 }
 
